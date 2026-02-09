278bdf38647822e8f9f5ed6388c642490f463b2b
OlraCode <cemcosta08@gmail.com>
2026-02-09 19:25:23 -0300

    feat: adicionar CRUD de blogs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 const routes = require('./routes/index');
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use('/api', routes);
 
 module.exports = app;
--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,21 +1,75 @@
-const index = (req, res) => {
-    return res.status(200).json('OK');
+const Blog = require('../models/blog.model');
+
+const index = async (req, res) => {
+    try {
+        const blogs = await Blog.find({});
+
+        if (blogs.length === 0) {
+            return res.status(200).json({message: "There's no blog"});
+        }
+
+        return res.status(200).json(blogs);
+
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
 }
 
-const show = (req, res) => {
-    return res.status(200).json('OK');
+const show = async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+
+        if (!blog) {
+            return res.status(404).json({message: "Blog not found"});
+        }
+
+        return res.status(200).json(blog);
+
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
 }
 
-const create = (req, res) => {
-    return res.status(200).json('OK');
+const create = async (req, res) => {
+    try {
+        const newBlog = await Blog.create(req.body);
+        return res.status(201).json(newBlog);
+
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
 }
 
-const update = (req, res) => {
-    return res.status(200).json('OK');
+const update = async (req, res) => {
+    try {
+        const blog = await Blog.findByIdAndUpdate(req.params.id, req.body);
+        
+        if (!blog) {
+            return res.status(404).json({message: "Blog not found"});
+        }
+
+        const updatedBlog = await Blog.findById(blog.id);
+
+        return res.status(200).json(updatedBlog);
+
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
 }
 
-const remove = (req, res) => {
-    return res.status(200).json('OK');
+const remove = async (req, res) => {
+    try {
+        const blog = await Blog.findByIdAndDelete(req.params.id);
+    
+        if (!blog) {
+            return res.status(404).json({message: "Blog not found"});
+        }
+    
+        return res.status(200).json({message: "Blog deleted"});
+
+    } catch (e) {
+        return res.status(500).json({error: e.message});
+    }
 }
 
 module.exports = {index, show, create, update, remove};
